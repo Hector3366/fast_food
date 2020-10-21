f1c07ea6f8f530e8e98856a8dbb24effc0369250
Hector3366 <javro363@gmail.com>
2020-10-21 17:43:47 -0500

    update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,114 +1,112 @@
 import 'package:fast_food/screens/welcome.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String nombre = "";
+  final nombreController = TextEditingController();
+
+  @override
+  void dispose() {
+    nombreController.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            /* floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Welcome()),
-                );
-              },
-            ),*/
-            appBar: AppBar(
-              backgroundColor: Colors.redAccent,
-              title: Text("Login"),
-              leading: Builder(
-                builder: (BuildContext context) {
-                  return IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Welcome()),
-                        );
-                      });
-                },
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text("Login"),
+          leading: Builder(
+            builder: (BuildContext context) {
+              return IconButton(
+                  icon: const Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Welcome()),
+                    );
+                  });
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            Image(
+              image: AssetImage('assets/images/auten.png'),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Text(
+                  'Ingresa tus credenciales \naceptadas para acceder',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'popins',
+                    decoration: TextDecoration.none,
+                    color: Colors.black87,
+                  ),
+                ),
               ),
             ),
-            body: Column(
-              children: [
-                Image(
-                  image: AssetImage('assets/images/auten.png'),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30, right: 20, left: 20),
+                child: TextField(
+                  controller: nombreController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(15))),
+                      hintText: 'Email',
+                      prefixIcon: Icon(Icons.mail),
+                      hintStyle:
+                          TextStyle(fontSize: 14.0, color: Colors.black26)),
                 ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Text(
-                      'Ingresa tus credenciales \naceptadas para acceder',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'popins',
-                        decoration: TextDecoration.none,
-                        color: Colors.black87,
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 25, right: 20, left: 20),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15)),
                       ),
+                      hintText: 'Contraseña',
+                      prefixIcon: Icon(Icons.enhanced_encryption)),
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 40, right: 30, left: 30),
+                child: TextButton(
+                  onPressed: () {
+                    nombre = nombreController.text;
+                    print("El nombre es: $nombre");
+                  },
+                  child: Text(
+                    'CONTINUAR',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'popins',
+                      color: Colors.white,
+                      fontSize: 20,
                     ),
                   ),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.redAccent)),
                 ),
-                Center(
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 30, right: 20, left: 20),
-                    child: TextField(
-                      //obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15))),
-                        labelText: 'Email',
-                        prefixIcon: Icon(Icons.email),
-                      ),
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.only(top: 25, right: 20, left: 20),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(15)),
-                          ),
-                          hintText: 'Contraseña',
-                          prefixIcon: Icon(Icons.enhanced_encryption)),
-                    ),
-                  ),
-                ),
-                Center(
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 40),
-                    child: FlatButton(
-                      minWidth: 365.0,
-                      height: 60.0,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20)),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Login()),
-                        );
-                      },
-                      color: Colors.red,
-                      child: Text(
-                        'CONTINUAR',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontFamily: 'popins',
-                          color: Colors.white,
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            )));
+              ),
+            ),
+          ],
+        )));
   }
 }
